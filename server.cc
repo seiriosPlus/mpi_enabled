64dbaf125fbcea4b4b12c0b23c8f7a88be2952ad
tangwei12 <tangwei12@baidu.com>
2018-04-18 11:17:21 +0800

    update code

--- server.cc
+++ server.cc
@@ -6,20 +6,22 @@
 #include <memory>
 #include <string>
 
+#include <grpc++/grpc++.h>
+
 #include "mpi_server.h"
 
 void RunServer() {
     std::string server_address("0.0.0.0:50051");
     paddle::operators::detail::MPIServerImpl service;
 
-    ServerBuilder builder;
+    grpc::ServerBuilder builder;
     // Listen on the given address without any authentication mechanism.
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
     // Register "service" as the instance through which we'll communicate with
     // clients. In this case it corresponds to an *synchronous* service.
     builder.RegisterService(&service);
     // Finally assemble the server.
-    std::unique_ptr <Server> server(builder.BuildAndStart());
+    std::unique_ptr <grpc::Server> server(builder.BuildAndStart());
     std::cout << "Server listening on " << server_address << std::endl;
 
     // Wait for the server to shutdown. Note that some other thread must be
